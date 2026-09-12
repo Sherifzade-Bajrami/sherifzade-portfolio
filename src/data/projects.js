75f87d8d4fdf7eac1eb78d383720b47f120d1017
Sherifzade-Bajrami <93668581+Sherifzade-Bajrami@users.noreply.github.com>
2026-09-12 18:58:37 +0200

    Refresh project showcase assets

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,6 @@
-import highwattage1 from "../assets/projects/highwattage-1.png";
-import highwattage2 from "../assets/projects/highwattage-2.png";
-import highwattage3 from "../assets/projects/highwattage-3.png";
-
-import swisssuite1 from "../assets/projects/swisssuite-1.png";
-import swisssuite2 from "../assets/projects/swisssuite-2.png";
 
 const customThemeScreenshots = [
-  highwattage1,
-  highwattage2,
-  highwattage3,
-  swisssuite1,
-  swisssuite2,
+
 ];
 
 export default customThemeScreenshots;
